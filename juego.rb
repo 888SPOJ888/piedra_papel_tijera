8cad5631044145ff19ee6de46b2ef7eb3cdf8c4b
alexis Montealegre <ajmu26@gmail.com>
2020-06-24 00:04:29 -0400

    almost ready

--- juego.rb
+++ juego.rb
@@ -1,1 +1,44 @@
-hola
+player = ARGV[0]
+unless (player == 'piedra' || player == 'papel' || player == 'tijera')
+    puts 'Eeepa eso no se vale!!! solo es piedra, papel y tijera'
+    
+else
+    comp = Random.rand(0..2)
+    if player == 'piedra'
+        if comp == 0
+            puts 'computador juega piedra'
+            puts 'empate'
+        elsif comp == 1
+            puts 'computador juega papel'
+            puts 'perdiste'
+        else 
+            puts 'computador juega tijera'
+            puts 'ganaste'
+    end
+end
+
+if player == 'papel'
+    if comp == 0
+        puts 'computador juega piedra'
+        puts 'ganaste'
+    elsif comp == 1
+        puts 'computador juega papel'
+        puts 'empate'
+    else 
+        puts 'computador juega tijera'
+        puts 'perdiste'
+end
+end
+if player == 'tijera'
+    if comp == 0
+        puts 'computador juega piedra'
+        puts 'perdiste'
+    elsif comp == 1
+        puts 'computador juega papel'
+        puts 'ganaste'
+    else 
+        puts 'computador juega tijera'
+        puts 'empate'
+end
+end
+end
